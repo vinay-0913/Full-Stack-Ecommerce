03e7ca7cb8fb288ae5141d691f8e3bce43b4a98c
vinay-0913 <vinayratnakar2003@gmail.com>
2025-07-01 23:13:43 +0530

    first commit

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { assets } from '../assets/assets'
 import axios from 'axios'
 import { backendUrl } from '../App'
-import { toast } from 'react-toastify'
+import { toast } from 'react-toastify' 
 
 const Add = ({ token }) => {
 
